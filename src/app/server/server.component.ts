e0e1c949d7b3187b0747e6ad2671d44c0e1fc7a2
Chirag Kalia <chiragkalia1@gmail.com>
2019-12-24 18:13:26 +0530

    Added DataBinding
    Identification: 234453
    Change-Id: If5200625b98636b06fb039fce49f96770dcaf0f5

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles:[`span{ color: green }
+    `]
 })
-export class ServerComponent{
+export class ServerComponent implements OnInit{
+    ServerNumber = 10;
+    ServerStatus = 'Online';
+    IsServerDisabled = true;
 
+    constructor(){
+        setTimeout(() => {
+            this.IsServerDisabled = false;
+        } , 2000); //Enable button after two seconds.
+    }
+
+    ngOnInit(): void {
+                    throw new Error("Method not implemented.");
+                }
 }
