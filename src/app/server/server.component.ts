c9c40ab4a3b0d4bdc26ddf3aa4738f1709e0c375
Chirag Kalia <chiragkalia1@gmail.com>
2019-12-26 15:21:33 +0530

    Added Event Binding and DataBinding
    Identification:237899
    Change-Id: I043f5bd91ba35d047d85dfcc43fa738ed45f4242

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,13 +10,20 @@ export class ServerComponent implements OnInit{
     ServerNumber = 10;
     ServerStatus = 'Online';
     IsServerDisabled = true;
-
+    ServerCreationStatus = "No Server Was Created!";
+    ServerName = '';
     constructor(){
         setTimeout(() => {
             this.IsServerDisabled = false;
         } , 2000); //Enable button after two seconds.
     }
 
+    OnServerCreated(){
+        this.ServerCreationStatus = "One Server Was Created!";
+    }
+    OnServerNameUpdate(event: Event){
+        this.ServerName = (<HTMLInputElement>event.target).value;
+    }
     ngOnInit(): void {
                     throw new Error("Method not implemented.");
                 }
