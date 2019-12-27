62de62c7da82627617ff04e30307f9791c2369cf
Chirag Kalia <chiragkalia1@gmail.com>
2019-12-27 15:55:34 +0530

    Added ng-Style directive
    Identification: 787878
    Change-Id: I15c2102c102146f2d2b35215628d365ed992a7e4

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { read } from 'fs';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html',
-    styles:[`span{ color: green }
-    `]
+    templateUrl: './server.component.html'
 })
 export class ServerComponent{
     ServerNumber = 10;
@@ -13,12 +12,16 @@ export class ServerComponent{
     ServerCreationStatus = "No Server Was Created!";
     ServerName = "";
     constructor(){
-
+        this.ServerStatus = Math.random() > 0.5 ? 'Online' : 'Offline'
     }
 
     OnServerCreated(){
         this.ServerCreated = true;
         this.ServerCreationStatus = "One Server Was Created! Server Name is "+ this.ServerName;
+    }
+
+    getColor(){
+        return this.ServerStatus === 'Online' ? 'green' : 'red';
     }
 
 
